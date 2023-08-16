2dc5c10ba060e64a83ba166b57771bcb13d50914
Kris Hvattum <klhvattum@live.com>
2023-08-15 19:53:42 -0700

    modify vite.config.js for ghpages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/',
+    base: '/vite-deploy-demo/',
     plugins: [react()],
     css: {
         preprocessorOptions: {

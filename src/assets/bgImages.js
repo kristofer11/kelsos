d304a93a96b771aff75cad6d81b0812ac34b3e6a
Kris Hvattum <klhvattum@live.com>
2023-08-15 17:37:52 -0700

    add image to bgImages

--- src/assets/bgImages.js
+++ src/assets/bgImages.js
@@ -3,7 +3,8 @@ import Tomahawk from '../assets/images/tomahawk.jpg';
 import Rosemary from '../assets/images/steakRosemary.jpg';
 import Hands from '../assets/images/michael-fousert-qbbUari4hsk-unsplash.jpg';
 import Sausage from '../assets/images/rich-smith-15tRu0OgPUk-unsplash.jpg';
-import Cow from '../assets/images/168DE641-BBAB-44CB-91AB-3236947A8A1A_1_105_c.jpeg'
+import Cow from '../assets/images/168DE641-BBAB-44CB-91AB-3236947A8A1A_1_105_c.jpeg';
+import Storefront from '../assets/images/storefront.jpg'
 
 export const bgImages = [
     Cutting,
@@ -11,5 +12,6 @@ export const bgImages = [
     Cow,
     Rosemary,
     Hands,
+    Storefront
 ]
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,15 @@ const Footer = () => {
     return (
         <footer className='footer'>
             <div>
+
                 <p>&#169; Kelso's Kustom Meats 2023</p>
 
                 <a href='/'>
                     <p>216 Maple Ave. Snohomish, WA 98290</p>
                 </a>
+                <p>Phone: <a href='tel: 360-568-3065'>(360) 568-3065</a></p>
             </div>
+
         </footer>
     )
 }

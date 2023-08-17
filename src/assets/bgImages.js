047e1b5ec8fb9c6a5329fb06a033fe589dc8324d
Kris Hvattum <klhvattum@live.com>
2023-08-16 17:25:51 -0700

    add to bg images

--- src/assets/bgImages.js
+++ src/assets/bgImages.js
@@ -2,7 +2,6 @@ import Cutting from '../assets/images/cuttingTomahawk.jpg';
 import Tomahawk from '../assets/images/tomahawk.jpg';
 import Rosemary from '../assets/images/steakRosemary.jpg';
 import Hands from '../assets/images/michael-fousert-qbbUari4hsk-unsplash.jpg';
-import Sausage from '../assets/images/rich-smith-15tRu0OgPUk-unsplash.jpg';
 import Cow from '../assets/images/168DE641-BBAB-44CB-91AB-3236947A8A1A_1_105_c.jpeg';
 import Storefront from '../assets/images/kelsos.jpg';
 import Case from '../assets/images/case.jpg'

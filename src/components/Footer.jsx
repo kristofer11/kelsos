5c0789f263df52f42f56cca64599d0d28f634a8b
Kris Hvattum <klhvattum@live.com>
2023-08-21 12:09:15 -0700

    add hours to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,40 +12,44 @@ const Footer = () => {
             </div>
 
             <div className='contactDiv' id='contactDiv'>
-                {/* <div>
-                    <a href='https://www.facebook.com/profile.php?id=100054302299730' target='_blank' >
-                        <img
-                            src={FbLogo}
-                            width='77'
-                        />
-                    </a>
-                </div> */}
 
                 <div className='addressDiv'>
                     <h4><strong>Kelso's Kustom Meats</strong></h4>
-                    <p>216 Maple Ave. Snohomish, WA 98290</p>
+                    <p>216 Maple Ave. </p>
+                    <p>Snohomish, WA 98290</p>
                     <p>Phone: <a href='tel: 360-568-3065'>(360) 568-3065</a></p>
                 </div>
 
-                <div className='krisDiv'>
+                <div className='hoursDiv'>
+                <h5>Hours</h5>
+                <ul>
+                    <li>Tuesday 9AM-6PM</li>
+                    <li>Wednesday: 9AM-6PM</li>
+                    <li>Thursday: 9AM-6PM</li>
+                    <li>Friday: 9AM-6PM</li>
+                    <li>Saturday: 9AM-5PM</li>
+                    <li>Sunday: <em>closed</em></li>
+                    <li>Monday: <em>closed</em></li>
+                </ul>
+            </div>
+
+            </div>
+            {/* <p style={{ margin: 0, paddingInline: '4rem' }}>&#169; Kelso's Kustom Meats 2023</p> */}
+            <div className='krisDiv'>
                     <p
-                        style={{ 
-                            fontSize: '1.5rem', 
-                            color: 'black', 
+                        style={{
+                            fontSize: '1.5rem',
+                            color: 'black',
 
                         }}
                     >
-                        Website by
+                        Website by {' '}
                         <a href='https://krishvattum.com' target='_blank' className='krisLink'>
-                            Kris Hvattum
+                            Kris Hvattum{' '}
                         </a>
                         2023
                     </p>
                 </div>
-
-
-            </div>
-            {/* <p style={{ margin: 0, paddingInline: '4rem' }}>&#169; Kelso's Kustom Meats 2023</p> */}
         </footer>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,7 +38,7 @@ const Header = () => {
                 <Navbar.Collapse id='navbarNav'>
                     <Nav>
                         <Nav.Link href="#mapDiv " style={{ fontSize: '2rem' }}>
-                            Find Us
+                            Address & Hours
                         </Nav.Link>
                         <Nav.Link href='https://www.facebook.com/profile.php?id=100054302299730' target='_blank' >
                             <img

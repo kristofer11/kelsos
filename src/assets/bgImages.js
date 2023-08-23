eb1d1d7fcd20b650ba0a1b46d8c2f2a6c21abad0
Kris Hvattum <klhvattum@live.com>
2023-08-22 18:25:16 -0700

    update bgImages

--- src/assets/bgImages.js
+++ src/assets/bgImages.js
@@ -3,15 +3,16 @@ import Tomahawk from '../assets/images/tomahawk.jpg';
 import Rosemary from '../assets/images/steakRosemary.jpg';
 import Hands from '../assets/images/michael-fousert-qbbUari4hsk-unsplash.jpg';
 import Cow from '../assets/images/168DE641-BBAB-44CB-91AB-3236947A8A1A_1_105_c.jpeg';
-import Storefront from '../assets/images/kelsos.jpg';
-import Case from '../assets/images/case.jpg'
+import Storefront from '../assets/images/kelsosStorefront.png';
+import Case from '../assets/images/case.jpg';
+import Liam from '../assets/images/liam.png'
 
 export const bgImages = [
     Cutting,
+    Liam,
     Hands,  
-    Storefront,  
-    Rosemary,      
+    Rosemary,
+    Storefront,           
     Cow,      
-    Tomahawk,
     Case,    
 ]

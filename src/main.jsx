970e552feb0c70518dce311aaf3ef4f217b23b61
Kris Hvattum <klhvattum@live.com>
2023-11-08 21:15:55 -0800

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { inject } from '@vercel/analytics';
+
+//ADD VERCEL ANALYTICS
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

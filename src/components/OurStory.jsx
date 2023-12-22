20b9318956407f7a7f86172a4ce1cf457dfa1304
Kris Hvattum <klhvattum@live.com>
2023-12-21 18:10:29 -0800

    style body phone link

--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -1,4 +1,5 @@
 import Liam from '../assets/images/liam-cropped.png';
+import Phone from '../assets/images/icons8-phone.svg'
 
 const OurStory = () => {
     return (
@@ -13,7 +14,26 @@ const OurStory = () => {
                         <li><span className='check'>✔</span> Game processing </li>
                         <li><span className='check'>✔</span> Wide variety of hand-cut, fresh meats to suit your preferences</li>
                         <li><span className='check'>✔</span> Delicious selection of cured meats</li>
-                        <li><em><strong>Feel free to <button href='tel: 360-568-3065' className='callLink'>call ahead</button> to place your order</strong></em></li>
+                        <li className='d-flex justify-content-center align-items-center gap-2'>
+                            <span><em><strong>Feel free to call ahead to place your order</strong></em></span>
+                            <a className='d-md-block d-none bodyCallLink'  href='tel: 360-568-3065' >
+                                <img
+                                    src={Phone}
+                                    className='headerLink bodyLink'
+                                    width='52'
+                                />
+                            </a>
+                        
+                        </li>
+                        <li>
+                            <a className='d-md-none callLink'  href='tel: 360-568-3065' >
+                                <img
+                                    src={Phone}
+                                    className='headerLink bodyCallLink'
+                                    width='66'
+                                />
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </div>
